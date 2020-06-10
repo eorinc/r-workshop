bff393a8db16d3edc36151737af5889bf8a2a8d0
Mike Talbot <29737293+realmiketalbot@users.noreply.github.com>
2020-06-10 04:45:38 -0500

    Various changes

--- scripts/week2.R
+++ scripts/week2.R
@@ -1,16 +1,32 @@
-setwd("~/r-workshop/data")
+setwd("/home/rstudio/r-workshop/data")
 
-#using read.table
+#using read.table----
+##from disk
 lf1 <- read.table("lakefinderdata.csv", sep=",", header=T)
 
-#using read.xlsx
+##from a url
+url <- "https://raw.githubusercontent.com/eorinc/r-workshop/master/data/lakefinderdata.csv"
+lf1 <- read.table(url, sep=",", header=T)
+
+#using read.xlsx----
 library(openxlsx)
+
+##from disk
 lf2 <- read.xlsx("lakefinderdata.xlsx", sheet=1)
 
-#using readRDS
+##from a url
+url <- "https://raw.githubusercontent.com/eorinc/r-workshop/master/data/lakefinderdata.xlsx"
+lf2 <- read.xlsx(url, sheet=1)
+
+#using readRDS----
+##from diskurl
 lf3 <- readRDS("lakefinderdata.rds")
 
-#using RPostgreSQL
+##from a 
+url <- "https://raw.githubusercontent.com/eorinc/r-workshop/master/data/lakefinderdata.rds"
+lf3 <- readRDS(gzcon(url(url)))
+
+#using RPostgreSQL----
 library(rpostgis) #or library(RPostgreSQL)
 pg = dbDriver("PostgreSQL")
 
@@ -20,5 +36,4 @@ password <- rstudioapi::askForPassword("Please enter your password")
 con = dbConnect(pg, user=username, password=password,
                 host="orthanc.talbot.casa", port=5432, dbname="testdb")
 lf4 <- dbReadTable(con, "lakefinderdata")
-
 
